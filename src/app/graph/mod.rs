78804e365323bbc9d5f2b25984540faac15e37a5
CrazyCraftix <43807375+CrazyCraftix@users.noreply.github.com>
2024-03-22 12:18:39 +0100

    refactor graph structure

--- src/app/graph/mod.rs
+++ src/app/graph/mod.rs
@@ -16,49 +16,56 @@ pub enum ConnectionType {
     TwoWay,
 }
 
-#[derive(Default, serde::Serialize, serde::Deserialize)]
-struct ConnectedActivityNode {
-    activity_node: ActivityNode,
-    connections: std::collections::HashMap<MutexNodeId, ConnectionType>,
-}
-
 #[derive(Default, serde::Deserialize, serde::Serialize)]
 pub struct Graph {
-    activity_nodes: std::collections::HashMap<ActivityNodeId, ConnectedActivityNode>,
+    activity_nodes: std::collections::HashMap<ActivityNodeId, ActivityNode>,
     mutex_nodes: std::collections::HashMap<MutexNodeId, MutexNode>,
+
+    connections: std::collections::HashMap<(ActivityNodeId, MutexNodeId), ConnectionType>,
+
     next_activity_node_id: ActivityNodeId,
     next_mutex_node_id: MutexNodeId,
 }
 
 impl Graph {
+    fn resolve_connections<'a>(
+        connections: &'a std::collections::HashMap<(ActivityNodeId, MutexNodeId), ConnectionType>,
+        mutex_nodes: &'a std::collections::HashMap<MutexNodeId, MutexNode>,
+        activity_node_id: ActivityNodeId,
+    ) -> Vec<(&'a MutexNode, &'a ConnectionType)> {
+        connections
+            .iter()
+            .filter_map(|((id, mutex_node_id), connection_type)| {
+                if *id != activity_node_id {
+                    return None;
+                } else {
+                    mutex_nodes
+                        .get(mutex_node_id)
+                        .map(|mutex_node| (mutex_node, connection_type))
+                }
+            })
+            .collect::<Vec<_>>()
+    }
+
     pub fn draw(&mut self, ui: &mut egui::Ui) {
-        self.activity_nodes.iter_mut().for_each(|n| {
-            n.1.activity_node.draw(
-                ui,
-                &n.1.connections
-                    .iter()
-                    .filter_map(|connection| {
-                        if let Some(mutex_node) = self.mutex_nodes.get(connection.0) {
-                            Some((mutex_node, connection.1))
-                        } else {
-                            None
-                        }
-                    })
-                    .collect(),
-            )
-        });
+        self.activity_nodes
+            .iter_mut()
+            .for_each(|(&activity_node_id, activity_node)| {
+                activity_node.draw(
+                    ui,
+                    &Self::resolve_connections(
+                        &self.connections,
+                        &self.mutex_nodes,
+                        activity_node_id,
+                    ),
+                )
+            });
         self.mutex_nodes.iter_mut().for_each(|n| n.1.draw(ui));
     }
 
     pub fn add_activiy_node(&mut self, activity_node: ActivityNode) -> ActivityNodeId {
         let id = self.next_activity_node_id;
-        self.activity_nodes.insert(
-            id,
-            ConnectedActivityNode {
-                activity_node,
-                ..Default::default()
-            },
-        );
+        self.activity_nodes.insert(id, activity_node);
         self.next_activity_node_id = ActivityNodeId(id.0 + 1);
         id
     }
@@ -76,23 +83,23 @@ impl Graph {
         mutex_node_id: MutexNodeId,
         connection_type: ConnectionType,
     ) -> bool {
-        self.mutex_nodes.contains_key(&mutex_node_id)
-            && self.activity_nodes.get_mut(&activity_node_id).map_or(
-                false,
-                |connected_activity_node| {
-                    if let Some(existing_connection_type) =
-                        connected_activity_node.connections.get_mut(&mutex_node_id)
-                    {
-                        if *existing_connection_type != connection_type {
-                            *existing_connection_type = ConnectionType::TwoWay;
-                        }
-                    } else {
-                        connected_activity_node
-                            .connections
-                            .insert(mutex_node_id, connection_type);
-                    }
-                    true
-                },
-            )
+        if !self.mutex_nodes.contains_key(&mutex_node_id)
+            || !self.activity_nodes.contains_key(&activity_node_id)
+        {
+            return false;
+        }
+
+        match self.connections.get_mut(&(activity_node_id, mutex_node_id)) {
+            Some(existing_connection_type) => {
+                if *existing_connection_type != connection_type {
+                    *existing_connection_type = ConnectionType::TwoWay;
+                }
+            }
+            None => {
+                self.connections
+                    .insert((activity_node_id, mutex_node_id), connection_type);
+            }
+        };
+        true
     }
 }
