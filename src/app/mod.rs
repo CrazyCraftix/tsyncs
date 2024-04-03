34445a89d4b29d2a9779020c6a84a73c81ec0b1e
MaZe <45102464+MaZe3D@users.noreply.github.com>
2024-04-03 02:36:12 +0200

    add initial setup

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,6 +1,6 @@
 use std::sync::mpsc::{channel, Receiver, Sender};
 
-use egui::{Button, Layout};
+use egui::{Button, Layout, Pos2, Rect};
 
 use self::graph::Graph;
 use std::future;
@@ -156,6 +156,8 @@ impl App {
             Some(storage) => eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default(),
             None => Default::default(),
         };
+
+        egui_extras::install_image_loaders(&creation_context.egui_ctx);
 
         creation_context
             .egui_ctx
@@ -415,6 +417,11 @@ impl eframe::App for App {
                 graphics::PanZoomContainer::new().show(
                     ui,
                     |ui, container_transform, container_response| {
+                        let image = egui::Image::new(egui::include_image!("../../assets/Logo.svg"));
+                        image.shrink_to_fit().paint_at(
+                            ui,
+                            egui::Rect::from_two_pos(Pos2::new(0., 0.), Pos2::new(120., 60.)),
+                        );
                         transform = container_transform;
                         self.graph.tick(ui);
                         self.graph
