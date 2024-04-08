48bc38452cc27eb9a4d99b40fde1f0c36a1bb66d
MaZe <45102464+MaZe3D@users.noreply.github.com>
2024-04-08 22:41:21 +0200

    screen size

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ fn main() -> eframe::Result<()> {
 
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([800.0, 600.0])
-            .with_min_inner_size([800.0, 600.0])
+            .with_inner_size([1200.0, 600.0])
+            .with_min_inner_size([1200.0, 600.0])
             .with_icon(
                 // NOE: Adding an icon is optional
                 eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
